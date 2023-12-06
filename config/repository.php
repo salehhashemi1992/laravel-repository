376764292c69cefbc7ddc2f17608bb66862f3264
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-12-06 14:19:08 +0330

    perpage config description

--- config/repository.php
+++ config/repository.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Pagination Settings
+    |--------------------------------------------------------------------------
+    |
+    | The 'limit' value acts as the default number of items per page when 'perPage' is passed as null.
+    | It also serves as the maximum limit for 'perPage' to control pagination size.
+    | Adjust this value to set the default and maximum items displayed per page.
+    |
+    */
     'perPage' => 20,
 ];
