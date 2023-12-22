cbeeb46689e363f75505e4d1ee39c2aa58066e54
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2023-12-23 00:24:29 +0330

    implement tests for preparePageSize method

--- tests/RepositoryTest.php
+++ tests/RepositoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Salehhashemi\Repository\Tests;
 
+use Illuminate\Contracts\Pagination\Paginator;
 use Salehhashemi\Repository\Tests\TestSupport\Models\Post;
 use Salehhashemi\Repository\Tests\TestSupport\Repositories\PostRepository;
 use Salehhashemi\Repository\Tests\TestSupport\Repositories\PostRepositoryInterface;
@@ -28,5 +29,33 @@ class RepositoryTest extends BaseTest
 
         $this->assertInstanceOf(Post::class, $foundPost);
         $this->assertEquals($post->id, $foundPost->id);
+    }
+
+    public function testPaginateWithSpecifiedPerPage()
+    {
+        Post::factory()->count(15)->create();
+
+        $postRepo = $this->app->make(PostRepositoryInterface::class);
+
+        $perPage = 5;
+        $paginatedResults = $postRepo->paginate($perPage);
+
+        $this->assertInstanceOf(Paginator::class, $paginatedResults);
+        $this->assertCount($perPage, $paginatedResults->items());
+        $this->assertEquals(1, $paginatedResults->currentPage());
+        $this->assertEquals(3, $paginatedResults->lastPage());
+    }
+
+    public function testPaginateWithDefaultPerPage()
+    {
+        Post::factory()->count(10)->create();
+
+        $postRepo = $this->app->make(PostRepositoryInterface::class);
+
+        $defaultPerPage = 20;
+        $paginatedResults = $postRepo->paginate();
+
+        $this->assertInstanceOf(Paginator::class, $paginatedResults);
+        $this->assertCount(min(10, $defaultPerPage), $paginatedResults->items());
     }
 }
