1e4fb3860cd98e9c72169578a9f80f5c3f796dc1
salehhashemi1992 <81674631+salehhashemi1992@users.noreply.github.com>
2024-03-07 16:21:03 +0330

    implement make repository command

--- config/repository.php
+++ config/repository.php
@@ -3,6 +3,18 @@
 return [
     /*
     |--------------------------------------------------------------------------
+    | Repository Paths
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the path where your repositories are located. This allows the
+    | application to dynamically resolve repository paths based on your configuration,
+    | making it easy to organize and manage your application's data access layer.
+    |
+    */
+    'path' => app_path('Repositories'), // Default repository path
+
+    /*
+    |--------------------------------------------------------------------------
     | Default Pagination Settings
     |--------------------------------------------------------------------------
     |
